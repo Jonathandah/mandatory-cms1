c0db727c94e66c54af724c248aa8ad40834aae52
unknown <jmd119988@gmail.com>
2019-09-01 12:13:58 +0200

    added styling to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import axios from "axios";
 import uuidv1 from "uuid/v1";
+import "../css/Home.css";
 
 function listPosts(post) {
-  return <li key={uuidv1()} className="Home__list__post"></li>;
+  return (
+    <li key={uuidv1()} className="Home__list__post">
+      <h3 className="Home__list__post__title">{post.Title}</h3>
+      <p className="Home__list__post__auther">{post.Auther}</p>
+      <p className="Home__list__post__date">{post.Date}</p>
+      {/*<p className="Home__list__post__text">{post.Post}</p>*/}
+    </li>
+  );
 }
 
 function Home() {

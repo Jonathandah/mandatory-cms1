e1ae4b7e4790a8e66cee1118ba553c3abca296b7
unknown <jmd119988@gmail.com>
2019-09-04 18:03:34 +0200

    aded new devspace-cockpit, new token and author can now be seen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import axios from "axios";
-import Home from "./components/Home";
-import Blog from "./components/Blog";
-import List from "./components/List";
-import "./css/App.css";
+import React, { useEffect, useState } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import List from './components/List';
+import './css/App.css';
 
 function App() {
   const [posts, updatePosts] = useState([]);
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        "http://localhost:8080/api/collections/get/Post?token=79e0270984c18ff8447814563d5a1f"
+        'https://cockpit-mandatory-cms1.devspace.host/api/collections/get/Article?token=42222d25d287392adc01146049c75b'
       )
       .then(response => {
         console.log(response);
@@ -26,7 +26,7 @@ function App() {
     console.log(id);
     axios
       .get(
-        `http://localhost:8080/api/collections/get/Post?token=79e0270984c18ff8447814563d5a1f&filter[_id]=${id}`
+        `https://cockpit-mandatory-cms1.devspace.host/api/collections/get/Article?token=42222d25d287392adc01146049c75b&filter[_id]=${id}`
       )
       .then(response => {
         console.log(response);
@@ -37,7 +37,7 @@ function App() {
   function getAuthor(id) {
     axios
       .get(
-        "http://localhost:8080/api/collections/get/Author?token=79e0270984c18ff8447814563d5a1f"
+        'https://cockpit-mandatory-cms1.devspace.host/api/collections/get/Author?token=42222d25d287392adc01146049c75b'
       )
       .then(response => {
         console.log(response);
@@ -47,23 +47,23 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <header className="App__header">{}</header>
-        <main className="App__main">
+      <div className='App'>
+        <header className='App__header'>{}</header>
+        <main className='App__main'>
           <Route
             exact
-            path="/"
+            path='/'
             render={() => <Home posts={posts} getArticle={getArticle} />}
           />
           {selectedPost.data ? (
             <Route
-              path="/blog/:id"
+              path='/blog/:id'
               render={() => <Blog selectedPost={selectedPost} />}
             />
           ) : null}
-          <Route path="/list" component={List} />
+          <Route path='/list' component={List} />
         </main>
-        <footer className="App__footer">{}</footer>
+        <footer className='App__footer'>{}</footer>
       </div>
     </Router>
   );
